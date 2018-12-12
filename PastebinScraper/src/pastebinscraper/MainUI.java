720e3c424348eda5d33a5da0baa29ae5a467a868
Kevin <kevinkrauel@gmail.com>
2018-12-12 15:57:29 -0600

    added all comments

--- PastebinScraper/src/pastebinscraper/MainUI.java
+++ PastebinScraper/src/pastebinscraper/MainUI.java
@@ -1,5 +1,5 @@
 package pastebinscraper;
-
+//massive amount of import library that make this functional.
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,12 +16,16 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
+//kevin jakson
 public class MainUI {
 	public MainUI(){
+                //creates the new title for the label of the program. 
 		title = new Label("Pastebin Scraper");
+                //sets the format
 		title.setFont(TITLE_FONT);
+                //ask for the url
 		paste_url_label = new Label("Paste URL: ");
+                //keywords
 		keywords_label = new Label("Keywords: ");
 		paste_url_field = new TextField();
 		keywords_field = new TextField();
@@ -32,11 +36,13 @@ public class MainUI {
 		VBox pane = new VBox(STD_SPACING);
 		pane.setPadding(STD_INSETS);
 		pane.setPrefWidth(400);
-		
+		//the hbox to show the url feild. 
 		HBox paste_url_group = new HBox(STD_SPACING);
+                //get the url label and teh information feild
 		paste_url_group.getChildren().addAll(paste_url_label, paste_url_field);
-		
+		//it is the keyword hbox
 		HBox keywords_group = new HBox(STD_SPACING);
+                //gets the keyword label. 
 		keywords_group.getChildren().addAll(keywords_label, keywords_field);
 
 		scrape_button.setOnAction(new EventHandler<ActionEvent>() {
@@ -45,30 +51,36 @@ public class MainUI {
 				doScrapeClick();
 			}
 		});
-		
+		//adds the title, url, keywords, and the botton to the pane. 
 		pane.getChildren().addAll(title, paste_url_group, keywords_group, scrape_button);
 		return pane;
 	}
 	
 	public void doScrapeClick(){
+                //This is the url that it would like to scrape
 		String scrape_url = paste_url_field.getText();
+                //List of strings of all the keywords. 
 		List<String> keywords = Arrays.asList(keywords_field.getText().split(","));
-		
+		//this will be the new paste data feild. 
 		Paste paste = new Paste(scrape_url);
+                //List of the paste results will search the list for keywords. 
 		List<PasteResult> paste_results = paste.search(keywords);
 				
 		spawnPasteResultsWindow(paste_results);
 	}
 	
 	private void spawnPasteResultsWindow(List<PasteResult> results){
+                //creates the new Stage for the results ui
 		Stage stage = new Stage();
+                //All the results. 
 		PasteResultsUI paste_results_ui = new PasteResultsUI(results);
 		Scene scene = new Scene(paste_results_ui.getPane(), 500, 500);
+                //sets the title of results window. 
 		stage.setTitle("Scrape Results");
 		stage.setScene(scene);
 		stage.show();
 	}
-	
+	//sets the font and the size. 
 	public static final Font TITLE_FONT = new Font("Arial", 18.0);
 	public static final Font STD_FONT = new Font("Arial", 14.0);
 	public static final Font CODE_FONT = new Font("Courier", 14.0);
