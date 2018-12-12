af42a813ee613d0ce8c87f66719ce0cb80c0da14
Kevin <kevinkrauel@gmail.com>
2018-12-12 15:58:00 -0600

    added all comments

--- PastebinScraper/src/pastebinscraper/Paste.java
+++ PastebinScraper/src/pastebinscraper/Paste.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-
+//kevin jakson
 public class Paste {
     
     public Paste(String url){
@@ -17,31 +17,39 @@ public class Paste {
     public final void readPaste(){
         
 		try{
+            //sets the url to the raw pastebin data. 
             URL website = new URL("https://pastebin.com/raw/" + getPasteID());
+            //this is the reader that will read the stream of website data. 
             BufferedReader reader = new BufferedReader(new InputStreamReader(website.openStream()));
-            
+                        //normal input
 			String input;
+            //will store the input of the reader line by line into the input varible. 
             while((input = reader.readLine())!=null){
+                //adds the input.
                 lines.add(input);
             }
+            //will throw an error if it can not connect to a webiste. 
         } catch(IOException ex){
             System.out.println("Could not read paste: " + ex.getMessage());
         }
     }
-    
+    //Search function 
     public List<PasteResult> search(List<String> keys){
+        //Search Result is where the result will be stored in the type of paste results. 
         List<PasteResult> search_results = new ArrayList<>();
         
 		for(int i = 0; i < lines.size(); i++){
+                        
 			String line = lines.get(i);
 			
 			for(int j = 0; j < keys.size(); j++){
+                             // checks to see if the line of input contians the key
 				if(line.contains(keys.get(j))){
 					PasteResult result = new PasteResult();
 					
 					// i + 1 for readability
 					result.line_number = i + 1;
-					
+					//will set the results to the corisponding line number
 					if(i == 0){
 						result.context[0] = "[Start of File]";
 					}else{
@@ -49,7 +57,7 @@ public class Paste {
 					}
 					
 					result.context[1] = line;
-					
+					//will set the two line of the results. 
 					if(i == (lines.size() - 1)){
 						result.context[2] = "[End of File]";
 					}else{
@@ -65,6 +73,7 @@ public class Paste {
     }
 	
 	public String getPasteID(){
+                //gets the end of the paste bin url to check that pastebin site. 
 		if(paste_id.equals("")){
 			return "tDU7mmVv";
 		}
@@ -74,6 +83,7 @@ public class Paste {
 	
 	public static String parsePasteID(String url){
 		// https://pastebin.com/hvDQ9S31
+                //will split the pastebim to add the url and the raw part
 		String[] url_parts = url.split("/");
 		if(url_parts.length == 4){
 			return url_parts[3];
